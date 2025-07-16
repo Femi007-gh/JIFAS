a7e93a72a0810c50b7b5dca2e3e597f2cf0bb210
Femi007-gh <femibaj81@gmail.com>
2025-07-16 08:15:50 +0100

    fifth commit

--- .history/script_20250716081156.js
+++ .history/script_20250716081156.js
@@ -1,28 +1,3 @@
-const hamburger = document.getElementById("hamburger");
-const closeBtn = document.getElementById("close-btn");
-const navContent = document.getElementById("nav-content");
-
-hamburger.onclick = function () {
-  navContent.classList.add("show");
-  hamburger.style.display = "none";
-};
-
-closeBtn.onclick = function () {
-  navContent.classList.remove("show");
-  hamburger.style.display = "block";
-};
-
-window.onresize = function () {
-  if (window.innerWidth >= 980) {
-    navContent.classList.remove("show");
-    hamburger.style.display = "none";
-  } else {
-    if (!navContent.classList.contains("show")) {
-      hamburger.style.display = "block";
-    }
-  }
-};
-
 const title1 = document.getElementById("title1");
 const title2 = document.getElementById("title2");
 const title3 = document.getElementById("title3");
@@ -81,3 +56,29 @@ title3.addEventListener("click", () => {
 });
 
 document.getElementById("desc3");
+
+
+const hamburger = document.getElementById("hamburger");
+const closeBtn = document.getElementById("close-btn");
+const navContent = document.getElementById("nav-content");
+
+hamburger.onclick = function () {
+  navContent.classList.add("show");
+  hamburger.style.display = "none";
+};
+
+closeBtn.onclick = function () {
+  navContent.classList.remove("show");
+  hamburger.style.display = "block";
+};
+
+window.onresize = function () {
+  if (window.innerWidth >= 980) {
+    navContent.classList.remove("show");
+    hamburger.style.display = "none";
+  } else {
+    if (!navContent.classList.contains("show")) {
+      hamburger.style.display = "block";
+    }
+  }
+};
